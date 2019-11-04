a2df8511dedf2e97e5e62aa01b02b00e6f86a9f7
Leandro Alves Santos <lealsto82@gmail.com>
2019-11-04 02:02:12 -0300

    Method to reallocate the list to another memory position.

--- LSystem/Collections/Generics/SimpleList.cs
+++ LSystem/Collections/Generics/SimpleList.cs
@@ -8,9 +8,51 @@ namespace LSystem.Collections.Generics
     [Serializable]
     public sealed class SimpleList<T> : IList<T>
     {
-        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        #region [Properties]
 
-        public int Count => throw new NotImplementedException();
+        private T[] _items;
+
+        private int _capacity;
+
+        /// <summary>
+        /// Indicates whether the list can be resized.
+        /// </summary>
+        private bool Resizable
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Indicates the max size of the SimpleList.
+        /// </summary>
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                if (value < Count)
+                    throw new ArgumentException("Capacity cannot be smaller then the actual list size.", "Capacity");
+
+                if (!Resizable)
+                    throw new InvalidOperationException("To change the initial capacity, set Resizable to true.");
+
+                _capacity = value;
+
+                ReallocateData();
+            }
+        }
+
+        /// <summary>
+        /// The actual list's length.
+        /// </summary>
+        public int Count { get; set; }
+
+        #endregion
+
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public bool IsReadOnly => throw new NotImplementedException();
 
@@ -63,5 +105,28 @@ namespace LSystem.Collections.Generics
         {
             throw new NotImplementedException();
         }
+
+        #region [Private Methods]
+
+        /// <summary>
+        /// Reallocates the data to a new memory position.
+        /// This just happens when the memory already contains
+        /// items added.
+        /// </summary>
+        private void ReallocateData()
+        {
+            if (Count > 0)
+            {
+                T[] tempArr = new T[Capacity];
+                Array.Copy(_items, 0, tempArr, 0, Count);
+                _items = tempArr;
+            }
+            else
+            {
+                _items = new T[Capacity];
+            }
+        }
+
+        #endregion
     }
 }
