1da2ef81a59f7a6663c296745620eb54f917a547
Leandro Alves Santos <lealsto82@gmail.com>
2019-11-03 21:08:43 -0300

    - Project LSystem created - Project LSystemTest created - Classes SimpleList and SimpleListTest created

---
+++ LSystemTest/Collections/Generics/SimpleListTest.cs
@@ -0,0 +1,17 @@
+﻿using LSystem.Collections.Generics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LSystemTest.Collections.Generics
+{
+
+    [TestClass]
+    public class SimpleListTest
+    {
+    }
+}
---
+++ LSystemTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LSystemTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LSystemTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca2a4e08-2e0f-4682-b14c-d63b75094acb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
