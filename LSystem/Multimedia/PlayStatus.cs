a0fdfd6ee4bf53172efe5098676a9e53e9de4e24
Leandro Alves Santos <lealsto82@gmail.com>
2019-11-21 02:41:04 -0300

    Enum with the file status

---
+++ LSystem/Multimedia/PlayStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace LSystem.Multimedia
+{
+    /// <summary>
+    /// Indicates wether the media file is in execution or paused or stopped.
+    /// </summary>
+    internal enum PlayStatus
+    {
+        Stopped = 0,
+        Playing = 1,
+        Paused = 2
+    }
+}
