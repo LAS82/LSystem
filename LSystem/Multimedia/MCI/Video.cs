bf0b4a96f9eb985046d656d6e55cab4a04937eb8
Leandro Alves Santos <lealsto82@gmail.com>
2019-12-15 19:24:24 -0300

    Video class and tests implemented

--- LSystem/Multimedia/MCI/Video.cs
+++ LSystem/Multimedia/MCI/Video.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LSystem.Drawing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,63 @@ using System.Threading.Tasks;
 
 namespace LSystem.Multimedia.MCI
 {
+
     public class Video : Media
     {
+        private Size _size;
+        private IntPtr _windowHandle;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mediaName">A reference name for the media file.</param>
+        /// <param name="filePath">The video file's path.</param>
+        public Video(string mediaName, string filePath)
+        {
+            base.MediaName = mediaName;
+            Open(filePath);
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mediaName">A reference name for the media file.</param>
+        /// <param name="filePath">The video file's path.</param>
+        /// <param name="size">A handle for a window to display the video.</param>
+        /// <param name="size">The window size.</param>
+        public Video(string mediaName, string filePath, IntPtr windowHandle, Size size)
+        {
+            this._size = size;
+            this._windowHandle = windowHandle;
+            base.MediaName = mediaName;
+            Open(filePath);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
         internal override void Open(string filePath)
         {
-            throw new NotImplementedException();
+            String command = String.Concat("open \"", filePath, "\" type mpegvideo alias MediaFile");
+
+            if (_windowHandle != IntPtr.Zero)
+            {
+                command = String.Concat(command, " style child parent ", this._windowHandle);
+                base.ExecuteMCICommand(command);
+                Put();
+            }
+            else
+                base.ExecuteMCICommand(command);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Put()
+        {
+            String command = String.Concat("put MediaFile window at 0 0 ", _size.Width, " ", _size.Height);
+            base.ExecuteMCICommand(command);
         }
     }
 }
---
+++ LSystemTest/Multimedia/MCI/VideoTest.cs
@@ -0,0 +1,45 @@
+﻿using LSystem.Multimedia;
+using LSystem.Multimedia.MCI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemTest.Multimedia.MCI
+{
+    [TestClass]
+    public class VideoTest
+    {
+        [TestMethod]
+        public void Play()
+        {
+            Video video = new Video("MediaFile", @"E:\HDD_General\Videos\Clipes e Shows\GoGo's - Head Over Heels.mpg");
+
+            Assert.AreEqual(PlayStatus.Stopped, video.MediaStatus);
+
+            //Play file.
+            video.Play(false);
+            Assert.AreEqual(PlayStatus.Playing, video.MediaStatus);
+
+            System.Threading.Thread.Sleep(15000);
+
+            //Pause file.
+            video.Pause();
+            Assert.AreEqual(PlayStatus.Paused, video.MediaStatus);
+
+            System.Threading.Thread.Sleep(2000);
+
+            //Resume file.
+            video.Resume();
+            Assert.AreEqual(PlayStatus.Playing, video.MediaStatus);
+
+            System.Threading.Thread.Sleep(15000);
+
+            //Close file.
+            video.Close();
+        }
+
+    }
+}
