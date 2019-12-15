acaca5223f79d535460bd5ee4b04ffcc96419728
Leandro Alves Santos <lealsto82@gmail.com>
2019-12-15 19:36:32 -0300

    Size structure improvements

--- LSystem/Drawing/Size.cs
+++ LSystem/Drawing/Size.cs
@@ -6,10 +6,56 @@ using System.Threading.Tasks;
 
 namespace LSystem.Drawing
 {
-    public struct Size
+    public struct Size : IEquatable<Size>
     {
         public int Width { get; set; }
 
         public int Height { get; set; }
+
+        public Size(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        /// <summary>
+        /// Compares the width and height properties.
+        /// </summary>
+        /// <param name="obj">Size object to compare.</param>
+        /// <returns>Comparation result.</returns>
+        public override bool Equals(object obj)
+        {
+            Size comparationSize = (Size)obj;
+
+            return this.Width == comparationSize.Width && this.Height == comparationSize.Height;
+        }
+
+        /// <summary>
+        /// Returns the hash code of this instance.
+        /// </summary>
+        /// <returns>Hach code.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns this instance values string formatted.
+        /// </summary>
+        /// <returns>Formatted values.</returns>
+        public override string ToString()
+        {
+            return String.Format("Width: {0}, Height: {1}", this.Width, this.Height);
+        }
+
+        /// <summary>
+        /// Compares the width and height properties.
+        /// </summary>
+        /// <param name="other">Size object to compare.</param>
+        /// <returns>Comparation result.</returns>
+        public bool Equals(Size other)
+        {
+            return this.Width == other.Width && this.Height == other.Height;
+        }
     }
 }
