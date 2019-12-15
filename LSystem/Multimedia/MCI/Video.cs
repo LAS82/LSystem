74d5ef1cb4db85557b7be4602666c35c3c2427bd
Leandro Alves Santos <lealsto82@gmail.com>
2019-12-15 18:13:11 -0300

    Video class added

---
+++ LSystem/Multimedia/MCI/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystem.Multimedia.MCI
+{
+    public class Video : Media
+    {
+        internal override void Open(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
