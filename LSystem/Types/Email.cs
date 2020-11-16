9ca9144bf5ca048cddaecaa13c03f95d8df7d029
Leandro Alves Santos <lealsto82@gmail.com>
2020-11-16 02:14:30 -0300

    Email data type created

---
+++ LSystem/Types/Email.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LSystem.Types
+{
+    /// <summary>
+    /// Structure to store an email address
+    /// </summary>
+    [DebuggerDisplay("Address = {Address}")]
+    public struct Email
+    {
+        #region [Constructor]
+
+        /// <summary>
+        /// Alternative constructor that already receives an address
+        /// </summary>
+        /// <param name="address">Email address</param>
+        public Email(string address) : this()
+        {
+            Address = address;
+        }
+
+        #endregion
+
+        #region [Properties]
+
+        public string _address { get; set; }
+        /// <summary>
+        /// Stores the address
+        /// </summary>
+        public string Address {
+            get
+            {
+                return _address;
+            }
+
+            set 
+            {
+                if (value != null)
+                    _address = value.Trim();
+                else
+                    throw new ArgumentException("Email address cannot be set to null", "address");
+            } 
+        }
+
+        /// <summary>
+        /// Check whether the email is valid.
+        /// </summary>
+        public bool IsValid {
+            get
+            {
+                return 
+                    Regex.IsMatch(
+                        Address, 
+                        @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", 
+                        RegexOptions.IgnoreCase
+                    );
+            }
+        }
+
+        #endregion
+
+        #region [Static Methods]
+
+        /// <summary>
+        /// Compares two addresses
+        /// </summary>
+        /// <param name="lEmail">Email</param>
+        /// <param name="rEmail">Email</param>
+        /// <returns>Comparison result</returns>
+        public static bool operator ==(Email lEmail, Email rEmail)
+        {
+            return Email.CheckEmailsAreEqual(lEmail, rEmail);
+        }
+
+        /// <summary>
+        /// Compares two addresses
+        /// </summary>
+        /// <param name="lEmail">Email</param>
+        /// <param name="rEmail">Email</param>
+        /// <returns>Comparison result</returns>
+        public static bool operator !=(Email lEmail, Email rEmail)
+        {
+            return !Email.CheckEmailsAreEqual(lEmail, rEmail);
+        }
+
+        /// <summary>
+        /// Check if both email addresses are equal.
+        /// </summary>
+        /// <param name="lEmail">Email</param>
+        /// <param name="rEmail">Email</param>
+        /// <returns>Comparison result</returns>
+        private static bool CheckEmailsAreEqual(Email lEmail, Email rEmail)
+        {
+            bool lEmailAddressIsNull = lEmail.CheckAddressIsNull();
+            bool rEmailAddressIsNull = rEmail.CheckAddressIsNull();
+
+            if (lEmailAddressIsNull && rEmailAddressIsNull)
+                return true;
+
+            if (lEmailAddressIsNull || rEmailAddressIsNull)
+                return false;
+
+            return lEmail.Address.ToLower() == rEmail.Address.ToLower();
+        }
+
+        #endregion
+
+        #region [Public Methods]
+
+        /// <summary>
+        /// Calls the base equals.
+        /// </summary>
+        /// <param name="obj">Object to compare</param>
+        /// <returns>Comparison result</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Returns the email address.
+        /// </summary>
+        /// <returns>Email address</returns>
+        public override string ToString()
+        {
+            return Address;
+        }
+
+        /// <summary>
+        /// Returns the hash code.
+        /// </summary>
+        /// <returns>Hash code.</returns>
+        public override int GetHashCode()
+        {
+            if (this.CheckAddressIsNull())
+                return base.GetHashCode();
+
+            return Address.GetHashCode();
+        }
+
+        #endregion
+
+        #region [Private Methods]
+
+        /// <summary>
+        /// Checks if the address was not yet set
+        /// </summary>
+        /// <returns>Result</returns>
+        private bool CheckAddressIsNull()
+        {
+            return this.Address == null;
+        }
+
+        #endregion
+
+    }
+}
---
+++ LSystemTest/Types/EmailTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LSystem.Types;
+
+namespace LSystemTest.Types
+{
+    [TestClass]
+    public class EmailTest
+    {
+        [TestMethod]
+        public void EmptyConstructor()
+        {
+            Email email1 = new Email();
+            Email email2 = new Email();
+
+            Assert.AreEqual(email1, email2);
+            Assert.IsTrue(email1 == email2);
+            Assert.IsFalse(email1 != email2);
+        }
+
+        [TestMethod]
+        public void ConstructorEqualAddresses()
+        {
+            Email email1 = new Email("email@valid.com");
+            Email email2 = new Email("email@valid.com");
+
+            Assert.AreEqual(email1, email2);
+            Assert.IsTrue(email1 == email2);
+            Assert.IsFalse(email1 != email2);
+        }
+
+        [TestMethod]
+        public void ConstructorDifferentAddresses()
+        {
+            Email email1 = new Email("firstemail@valid.com");
+            Email email2 = new Email("secondemail@valid.com");
+
+            Assert.AreNotEqual(email1, email2);
+            Assert.IsFalse(email1 == email2);
+            Assert.IsTrue(email1 != email2);
+        }
+
+        [TestMethod]
+        public void InvalidAddress()
+        {
+            Email email = new Email("invalid@.com");
+            Assert.IsFalse(email.IsValid);
+        }
+
+        [TestMethod]
+        public void ValidAddress()
+        {
+            Email email = new Email("   valid@valid.com   ");
+            Assert.IsTrue(email.IsValid);
+        }
+
+        [TestMethod]
+        public void NullAddress()
+        {            
+            Assert.ThrowsException<ArgumentException>(() => 
+                {
+                    Email email = new Email(null);
+                }, 
+                "Email address cannot be set to null");
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Email email = new Email("valid@email.com");
+            Assert.IsTrue(email.ToString() == "valid@email.com");
+        }
+    }
+}
