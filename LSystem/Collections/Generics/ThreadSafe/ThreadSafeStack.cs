e368d7120b89572f52140060c9b55c59fcce58f1
Leandro Alves Santos <lealsto82@gmail.com>
2020-12-13 00:45:43 -0300

    Thread Safe Stack implemented

---
+++ LSystem/Collections/Generics/ThreadSafe/ThreadSafeStack.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystem.Collections.Generics.ThreadSafe
+{
+    [Serializable]
+    [DebuggerDisplay("Count = {Count}")]
+    /// <summary>
+    /// A stack container implemented using a LinkedList
+    /// </summary>
+    /// <typeparam name="T">Any object</typeparam>
+    public class ThreadSafeStack<T>
+    {
+
+        #region [Properties]
+
+        /// <summary>
+        /// Data
+        /// </summary>
+        LinkedList<T> _items;
+
+        /// <summary>
+        /// Container length.
+        /// </summary>
+        public int Count => this._items.Count;
+
+        private object makeItAtomic = new object();
+
+        #endregion
+
+        #region [Constructors]
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ThreadSafeStack()
+        {
+            this._items = new LinkedList<T>();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="items">The items to initialize the container</param>
+        public ThreadSafeStack(IEnumerable<T> items) : this()
+        {
+            foreach (T item in items)
+                AddItem(item);
+        }
+
+        #endregion
+
+        #region [Public methods]
+
+        /// <summary>
+        /// Indicates wether the container is empty
+        /// </summary>
+        /// <returns>Indicates wether the container is empty</returns>
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        /// <summary>
+        /// Adds an item in the container
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        public void Push(T item)
+        {
+            lock (makeItAtomic)
+            {
+                this.AddItem(item);
+            }
+        }
+
+        /// <summary>
+        /// Adds a set of items in the container
+        /// </summary>
+        /// <param name="item">A list of items</param>
+        public void Push(IEnumerable<T> items)
+        {
+            lock (makeItAtomic)
+            {
+                foreach (T item in items)
+                    this.AddItem(item);
+            }
+        }
+
+        /// <summary>
+        /// Removes the top container's item
+        /// </summary>
+        /// <returns>Removed item</returns>
+        public T Pop()
+        {
+            T top;
+
+            lock (makeItAtomic)
+            {
+                if (this.IsEmpty())
+                {
+                    top = default(T);
+                }
+                else
+                {
+                    top = this.GetFirstItem();
+                    this.RemoveFirstItem();
+                }
+            }
+
+            return top;
+        }
+
+        /// <summary>
+        /// Returns the top item
+        /// </summary>
+        /// <returns>Top item</returns>
+        public T Top()
+        {
+            return this.GetFirstItem();
+        }
+
+        /// <summary>
+        /// Removes all container data
+        /// </summary>
+        public void Clear()
+        {
+            this._items.Clear();
+        }
+
+        #endregion
+
+        #region [Private methods]
+
+        /// <summary>
+        /// Adds an item in the linked list
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        private void AddItem(T item)
+        {
+            this._items.AddFirst(item);
+        }
+
+        /// <summary>
+        /// Removes the first item in the linked list
+        /// </summary>
+        private void RemoveFirstItem()
+        {
+            this._items.RemoveFirst();
+        }
+
+        /// <summary>
+        /// Gets the first item in the container
+        /// </summary>
+        /// <returns></returns>
+        private T GetFirstItem()
+        {
+            return this._items.First();
+        }
+
+        #endregion
+    }
+}
---
+++ LSystemTest/Collections/Generics/ThreadSafe/ThreadSafeStackTest.cs
@@ -0,0 +1,113 @@
+﻿using LSystem.Collections.Generics.ThreadSafe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemTest.Collections.Generics.ThreadSafe
+{
+    [TestClass]
+    public class ThreadSafeStackTest
+    {
+        [TestMethod]
+        public void TestAsyncPush()
+        {
+            ThreadSafeStack<int> integers = new ThreadSafeStack<int>();
+
+            Task t1 = Task.Run(
+                () => {
+                    for (int i = 0; i < 1000000; ++i)
+                    {
+                        integers.Push(i);
+                    }
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    for (int i = 0; i < 1000000; ++i)
+                    {
+                        integers.Push(i);
+                    }
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(2000000, integers.Count);
+        }
+
+        [TestMethod]
+        public void TestAsyncPushList()
+        {
+            ThreadSafeStack<int> integers = new ThreadSafeStack<int>();
+
+            Task t1 = Task.Run(
+                () => {
+                    List<int> data = new List<int>(500);
+                    for (int i = 0; i < 1000; i+=2)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        data.Add(i);
+                    }
+
+                    integers.Push(data);
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    List<int> data = new List<int>(500);
+                    for (int i = 1; i < 1000; i += 2)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        data.Add(i);
+                    }
+
+                    integers.Push(data);
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(1000, integers.Count);
+        }
+
+        [TestMethod]
+        public void TestAsyncPop()
+        {
+            ThreadSafeStack<int> integers = new ThreadSafeStack<int>();
+
+            for (int i = 3; i < 1000000; ++i)
+            {
+                integers.Push(i);
+            }
+
+            Task t1 = Task.Run(
+                () => {
+                    while(!integers.IsEmpty())
+                    {
+
+                        integers.Pop();
+                    }
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    while(!integers.IsEmpty())
+                    {
+                        integers.Pop();
+                    }
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(0, integers.Count);
+        }
+
+    }
+}
