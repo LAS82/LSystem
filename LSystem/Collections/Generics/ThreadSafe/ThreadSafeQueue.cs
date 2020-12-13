9155eb0b6fb95409eff24951233c3362ecd8b8ed
Leandro Alves Santos <lealsto82@gmail.com>
2020-12-12 23:46:14 -0300

    Thread Safe Queue implemented

---
+++ LSystem/Collections/Generics/ThreadSafe/ThreadSafeQueue.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystem.Collections.Generics.ThreadSafe
+{
+    public class ThreadSafeQueue<T>
+    {
+        #region [Properties]
+
+        /// <summary>
+        /// Data
+        /// </summary>
+        LinkedList<T> _items;
+
+        /// <summary>
+        /// Container length.
+        /// </summary>
+        public int Count => this._items.Count;
+
+        private object makeItAtomic = new object();
+
+        #endregion
+
+        #region [Constructors]
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ThreadSafeQueue()
+        {
+            this._items = new LinkedList<T>();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="items">The items to initialize the container</param>
+        public ThreadSafeQueue(IEnumerable<T> items) : this()
+        {
+            foreach (T item in items)
+                AddItem(item);
+        }
+
+        #endregion
+
+        #region [Public methods]
+
+        /// <summary>
+        /// Indicates wether the container is empty
+        /// </summary>
+        /// <returns>Indicates wether the container is empty</returns>
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        /// <summary>
+        /// Adds an item in the container
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        public void Enqueue(T item)
+        {
+            lock (makeItAtomic)
+            {
+                this.AddItem(item);
+            }
+        }
+
+        public void Enqueue(IEnumerable<T> items)
+        {
+            lock (makeItAtomic)
+            {
+                foreach(T item in items)
+                    this.AddItem(item);
+            }
+        }
+
+        /// <summary>
+        /// Removes the front container's item
+        /// </summary>
+        /// <returns>Removed item</returns>
+        public T Dequeue()
+        {
+            T front;
+
+            lock (makeItAtomic)
+            {
+                if (this.IsEmpty())
+                {
+                    front = default(T);
+                }
+                else
+                {
+                    front = this.GetFirstItem();
+                    this.RemoveFirstItem();
+                }
+            }
+
+            return front;
+        }
+
+        /// <summary>
+        /// Returns the front item
+        /// </summary>
+        /// <returns>Front item</returns>
+        public T Front()
+        {
+            return this.GetFirstItem();
+        }
+
+        /// <summary>
+        /// Removes all container data
+        /// </summary>
+        public void Clear()
+        {
+            this._items.Clear();
+        }
+
+        #endregion
+
+        #region [Private methods]
+
+        /// <summary>
+        /// Adds an item in the linked list
+        /// </summary>
+        /// <param name="item">item to be added</param>
+        private void AddItem(T item)
+        {
+            this._items.AddLast(item);
+        }
+
+        /// <summary>
+        /// Removes the first item in the linked list
+        /// </summary>
+        private void RemoveFirstItem()
+        {
+            this._items.RemoveFirst();
+        }
+
+        /// <summary>
+        /// Gets the first item in the container
+        /// </summary>
+        /// <returns></returns>
+        private T GetFirstItem()
+        {
+            return this._items.First();
+        }
+
+        #endregion
+    }
+}
---
+++ LSystemTest/Collections/Generics/ThreadSafe/ThreadSafeQueueTest.cs
@@ -0,0 +1,113 @@
+﻿using LSystem.Collections.Generics.ThreadSafe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemTest.Collections.Generics.ThreadSafe
+{
+    [TestClass]
+    public class ThreadSafeQueueTest
+    {
+        [TestMethod]
+        public void TestAsyncEnqueue()
+        {
+            ThreadSafeQueue<int> integers = new ThreadSafeQueue<int>();
+
+            Task t1 = Task.Run(
+                () => {
+                    for (int i = 0; i < 1000000; ++i)
+                    {
+                        integers.Enqueue(i);
+                    }
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    for (int i = 0; i < 1000000; ++i)
+                    {
+                        integers.Enqueue(i);
+                    }
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(2000000, integers.Count);
+        }
+
+        [TestMethod]
+        public void TestAsyncEnqueueList()
+        {
+            ThreadSafeQueue<int> integers = new ThreadSafeQueue<int>();
+
+            Task t1 = Task.Run(
+                () => {
+                    List<int> data = new List<int>(500);
+                    for (int i = 0; i < 1000; i+=2)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        data.Add(i);
+                    }
+
+                    integers.Enqueue(data);
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    List<int> data = new List<int>(500);
+                    for (int i = 1; i < 1000; i += 2)
+                    {
+                        System.Threading.Thread.Sleep(10);
+                        data.Add(i);
+                    }
+
+                    integers.Enqueue(data);
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(1000, integers.Count);
+        }
+
+        [TestMethod]
+        public void TestAsyncDequeue()
+        {
+            ThreadSafeQueue<int> integers = new ThreadSafeQueue<int>();
+
+            for (int i = 3; i < 1000000; ++i)
+            {
+                integers.Enqueue(i);
+            }
+
+            Task t1 = Task.Run(
+                () => {
+                    while(!integers.IsEmpty())
+                    {
+
+                        integers.Dequeue();
+                    }
+                }
+            );
+
+            Task t2 = Task.Run(
+                () => {
+                    while(!integers.IsEmpty())
+                    {
+                        integers.Dequeue();
+                    }
+                }
+            );
+
+            Task.WaitAll(t1, t2);
+
+            Assert.AreEqual(0, integers.Count);
+        }
+
+    }
+}
